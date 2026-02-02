cf327f8b7ac4d98e1d3815152d5965860c09ffbc
awesomergs <george.rohan21@gmail.com>
2026-02-02 10:59:53 -0800

    Initial Commit

---
+++ HeadsUp/HeadsUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeadsUpApp.swift
+//  HeadsUp
+//
+//  Created by Rohan George on 2/2/26.
+//
+
+import SwiftUI
+
+@main
+struct HeadsUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
